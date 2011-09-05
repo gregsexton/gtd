07d74639f20a86fc23570dd89f3d227cc4f93780
Greg Sexton <gregsexton@gmail.com>
2011-09-05 17:23:39 +0100

    Refactored logic in reflow.
    This is a work in progress.

--- reflow.clj
+++ reflow.clj
@@ -10,16 +10,6 @@
 (defn sum [coll]
   (reduce + 0 coll))
 
-;TODO: use some sort of destructuring?
-;TODO: is this going to be fast enough?? definitely not. 
-(defn parts [coll n]
-  (cond
-    (= n 1) (list (list (sum coll)))
-    (empty? coll) (list (repeat n 0))
-    :else (mapcat #(map (partial cons (sum (first %)))
-                        (parts (second %) (dec n)))
-                  (splits coll))))
-
 ;TODO: destructuring
 (defn min-item [f coll]
   (let [fs (map f coll)
@@ -32,8 +22,17 @@
   (- (apply max coll)
      (apply min coll)))
 
-(defn min-diff [colls]
-  (min-item part-diff colls))
+;TODO: sort out the constants
+;TODO: revise. is there any nicer way of writing this?
+;TODO: speed up! memoize? table?
+(defn parts [[fst & rst :as coll] n current-sum]
+  (cond 
+    (= n 1) (list (sum coll))
+    (empty? coll) '(10000 -10000) 
+    :else (let [sum (+ current-sum fst)]
+            (min-item part-diff 
+                      [(cons sum (parts rst (dec n) 0))
+                       (parts (rest coll) n sum)]))))
 
 (defn front-sum-count
   "How many values from the front of coll are required to sum to x."
@@ -57,9 +56,10 @@
                      (drop take-cnt values)
                      (rest take-coll)))))))
 
+;TODO: had to supply the starting zero to parts here. refactor.
 (defn even-partitions [coll f n]
   (let [fs (map f coll)
-        min-part (min-diff (parts fs n))]
+        min-part (parts fs n 0)]
     (takes coll fs min-part)))
 
 (defn words [str-input]
@@ -77,5 +77,5 @@
 
 (def reflow-test '("This" "is" "a" "really" "long" "sentence" "that" "should" "wrap" "and" "display" "in" "a" "smaller" "font." "This" "is" "a" "really" "long" "sentence" "that" "should" "wrap" "and" "display" "in" "a" "smaller" "font."))
 
-;(even-partitions reflow-test count 7) ;way slow
+(even-partitions reflow-test count 7) ;way slow
 ;(even-partitions reflow-test count 8) ;starting to disappear to never land
