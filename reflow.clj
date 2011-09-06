57b6df8eca5c446daaea896b7f68cff478fea20a
Greg Sexton <gregsexton@gmail.com>
2011-09-06 17:37:16 +0100

    Reworkd reflow to be fast and tidier.

--- reflow.clj
+++ reflow.clj
@@ -4,42 +4,57 @@
 ;intended exports:
   ;reflow
 
-(defn splits [coll]
-  (take (count coll) (map split-at (iterate inc 1) (repeat coll))))
+(defn last-rest
+  "Returns a seq of pairs made up of splitting the coll in all possible
+  places. The pair consists of the last element of the front half and
+  the entire back half."
+  [coll]
+  (lazy-seq
+    (if (empty? coll) []
+      (conj (last-rest (rest coll))
+            [(first coll) (rest coll)]))))
+
+(defn cum-last-rest
+  "Like last-rest but accumulates the first item in the pair."
+  [coll]
+  (letfn [(help [acc [[x xs] & ys]]
+            (lazy-seq
+              (if (empty? ys) [[(+ acc x) xs]]
+                (conj (help (+ acc x) ys)
+                      [(+ acc x) xs]))))]
+    (help 0 (last-rest coll))))
 
 (defn sum [coll]
   (reduce + 0 coll))
 
-;TODO: destructuring
 (defn min-item [f coll]
   (let [fs (map f coll)
         min-fs (apply min fs)
-        pairs (map #(vector %1 %2) fs coll)]
-    (second (first (drop-while #(not= (first %) min-fs)
-                               pairs)))))
+        pairs (map #(hash-map :f %1 :val %2) fs coll)]
+    (:val (first (drop-while #(not= (:f %) min-fs)
+                             pairs)))))
 
-(defn part-diff [coll]
-  (- (apply max coll)
-     (apply min coll)))
-
-;TODO: sort out the constants
-;TODO: revise. is there any nicer way of writing this?
-;TODO: speed up! memoize? table?
-(defn parts [[fst & rst :as coll] n current-sum]
-  (cond 
+;TODO: is there a more idiomatic way of declaring this memoization?
+(declare parts-mem)
+(defn parts
+  "Splits coll into n summations of linear partitions maintaining a
+  minimal difference across the partitoins."
+  [coll n]
+  (cond
     (= n 1) (list (sum coll))
-    (empty? coll) '(10000 -10000) 
-    :else (let [sum (+ current-sum fst)]
-            (min-item part-diff 
-                      [(cons sum (parts rst (dec n) 0))
-                       (parts (rest coll) n sum)]))))
+    (empty? coll) (list 0)
+    :else (min-item (partial apply max)
+                    (map #(conj (parts-mem (second %) (dec n))
+                                (first %))
+                         (cum-last-rest coll)))))
+(def parts-mem (memoize parts))
 
 (defn front-sum-count
   "How many values from the front of coll are required to sum to x."
   [coll x]
   (loop [coll coll
          acc 0 cnt 0]
-    (cond 
+    (cond
       (>= acc x) cnt
       (empty? coll) cnt
       :else (recur (rest coll) (+ acc (first coll)) (inc cnt)))))
@@ -56,10 +71,12 @@
                      (drop take-cnt values)
                      (rest take-coll)))))))
 
-;TODO: had to supply the starting zero to parts here. refactor.
-(defn even-partitions [coll f n]
+(defn even-partitions 
+  "Breaks coll into n partitions minimising the difference in score as
+  calculated by applying f to each element in coll."
+  [coll f n]
   (let [fs (map f coll)
-        min-part (parts fs n 0)]
+        min-part (parts fs n)]
     (takes coll fs min-part)))
 
 (defn words [str-input]
@@ -72,10 +89,3 @@
   [input breaks]
   (let [parts (even-partitions (words input) count breaks)]
     (map (partial join " ") parts)))
-
-;for testing only:
-
-(def reflow-test '("This" "is" "a" "really" "long" "sentence" "that" "should" "wrap" "and" "display" "in" "a" "smaller" "font." "This" "is" "a" "really" "long" "sentence" "that" "should" "wrap" "and" "display" "in" "a" "smaller" "font."))
-
-;(even-partitions reflow-test count 7) ;way slow
-;(even-partitions reflow-test count 8) ;starting to disappear to never land
