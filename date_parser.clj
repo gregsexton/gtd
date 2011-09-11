c81e8490d982aa4a86b04698c6124f961a0ab01d
Greg Sexton <gregsexton@gmail.com>
2011-09-11 19:36:41 +0100

    Date parsing: special case relative forms working.

--- date_parser.clj
+++ date_parser.clj
@@ -1,11 +1,54 @@
 (ns date-parser
   (:use [clojure.string :only (trim)])
+  (:use [clojure.contrib.cond])
   (:require [clojure.contrib.string :as s])
   (:import
      (java.util Calendar GregorianCalendar)))
 
 ;intended exports:
   ;parse-date
+
+;date utilities
+(defn create-date [year month date hour minute sec]
+  (let [cal (GregorianCalendar.)]
+    (.set cal year month date hour minute sec)
+    (.set cal Calendar/MILLISECOND 0)
+    (.getTime cal)))
+(defn get-year []
+  (.get (GregorianCalendar.) Calendar/YEAR))
+(defn get-month []
+  (.get (GregorianCalendar.) Calendar/MONTH))
+(defn get-day []
+  (.get (GregorianCalendar.) Calendar/DATE))
+(defn get-day-of-week []
+  (.get (GregorianCalendar.) Calendar/DAY_OF_WEEK))
+(defn days-until [day]
+  (defn help [day]
+    (mod (+ (- day (get-day-of-week)) 7) 7))
+  (let [mapping {:monday Calendar/MONDAY
+                 :tuesday Calendar/TUESDAY
+                 :wednesday Calendar/WEDNESDAY
+                 :thursday Calendar/THURSDAY
+                 :friday Calendar/FRIDAY
+                 :saturday Calendar/SATURDAY
+                 :sunday Calendar/SUNDAY}]
+    (help (mapping day))))
+(defn months-until [month]
+  (defn help [month]
+    (mod (+ (- month (get-month)) 12) 12))
+  (let [mapping {:january Calendar/JANUARY
+                 :february Calendar/FEBRUARY
+                 :march Calendar/MARCH
+                 :april Calendar/APRIL
+                 :may Calendar/MAY
+                 :june Calendar/JUNE
+                 :july Calendar/JULY
+                 :august Calendar/AUGUST
+                 :september Calendar/SEPTEMBER
+                 :october Calendar/OCTOBER
+                 :november Calendar/NOVEMBER
+                 :december Calendar/DECEMBER}]
+    (help (mapping month))))
 
 ;utilities
 (defn if-val
@@ -79,7 +122,7 @@
   "Returns map with keys [:number :rest] representing the matched number
   and the rest of the stream respectively. Returns nil if no match."
   [stream]
-  ;TODO: can this be refactored and made a lot nicer?
+  ;TODO: can this be refactored and made a lot nicer? cond-let?
   (defn match-one [stream] ;treat one specially due to synonyms
     (if-let [[match tail] (split-regex one-regex stream)]
       {:number 1 :rest tail}))
@@ -87,10 +130,52 @@
     (if-let [[match tail] (split-regex num-regex stream)]
       {:number (Integer. match) :rest tail})))
 
+(defmacro special-form-cond-let
+  "Takes a stream and a number of vectors for forms. Each vector should
+  contain a regex, a number and a token value. If the regex matches the
+  stream then a map is returned corresponding to the map expected of
+  match-special."
+  [stream & forms]
+  (when-let [[[regex number token] & more] forms]
+    `(if-let [tail# (drop-regex ~regex ~stream)]
+       {:number ~number :token ~token :rest tail#}
+       (special-form-cond-let ~stream ~@more))))
+
+(defn match-special
+  "Returns map with keys [:number :token :rest] representing the matched
+  number and token pair of the special form. :rest is the remaining
+  unmatched stream. Returns nil if no match."
+  [stream]
+  (special-form-cond-let stream
+                         [#"^tomorrow" 1 :day]
+                         [#"^now" 0      :second]
+                         [#"^-" 0        :second]
+                         [#"^\." 0       :second]
+                         [#"^(?:mon(?:day)?)"      (days-until :monday)    :day]
+                         [#"^(?:tues?(?:day)?)"    (days-until :tuesday)   :day]
+                         [#"^(?:weds?(?:nesday)?)" (days-until :wednesday) :day]
+                         [#"^(?:thur?(?:sday)?)"   (days-until :thursday)  :day]
+                         [#"^(?:fri(?:day)?)"      (days-until :friday)    :day]
+                         [#"^(?:sat(?:urday)?)"    (days-until :saturday)  :day]
+                         [#"^(?:sun(?:day)?)"      (days-until :sunday)    :day]
+                         [#"^(?:jan(?:uary)?)"   (months-until :january)   :month]
+                         [#"^(?:feb(?:ruary)?)"  (months-until :february)  :month]
+                         [#"^(?:mar(?:ch)?)"     (months-until :march)     :month]
+                         [#"^(?:apr(?:il)?)"     (months-until :april)     :month]
+                         [#"^(?:may)"            (months-until :may)       :month]
+                         [#"^(?:june?)"          (months-until :june)      :month]
+                         [#"^(?:july?)"          (months-until :july)      :month]
+                         [#"^(?:aug(?:ust)?)"    (months-until :august)    :month]
+                         [#"^(?:sep(?:tember)?)" (months-until :september) :month]
+                         [#"^(?:oct(?:ober)?)"   (months-until :october)   :month]
+                         [#"^(?:nov(?:ember)?)"  (months-until :november)  :month]
+                         [#"^(?:dec(?:ember)?)"  (months-until :december)  :month]))
+
 (defn tokenize [date-str]
   (lazy-seq
     (let [stream (drop-regex separator date-str)]
       (if (= date-str "") [:second 1]
+        ;TODO: refactor using cond-let?
         ;try and match a token
         (if-let [match (match-token stream)]
           (conj (tokenize (:rest match))
@@ -98,35 +183,27 @@
           ;try and match a number
           (if-let [match (match-number stream)]
             (conj (tokenize (:rest match))
-                  (:number match))))))))
+                  (:number match))
+            ;try and match a special form
+            (if-let [match (match-special stream)]
+              (conj (tokenize (:rest match))
+                    (:token match)
+                    (:number match)))))))))
 
 (defn get-unit [tokens]
   (first-of tokens keyword? :second))
 
 (defn get-time-value [tokens]
   (let [value (first-of tokens number? 0)]
-    (if (= :week (get-unit tokens)) 
+    (if (= :week (get-unit tokens))
       (* value 7) value)))
 
 (defn get-offset [date-str]
-  (let [tokens (tokenize date-str)]
-    {:unit (units (get-unit tokens))
-     :val  (get-time-value tokens)}))
-
-;date utilities
-(defn create-date [year month date hour minute sec]
-  (let [cal (GregorianCalendar.)]
-    (.set cal year month date hour minute sec)
-    (.set cal Calendar/MILLISECOND 0)
-    (.getTime cal)))
-(defn get-year []
-  (.get (GregorianCalendar.) Calendar/YEAR))
-(defn get-month []
-  (.get (GregorianCalendar.) Calendar/MONTH))
-(defn get-day []
-  (.get (GregorianCalendar.) Calendar/DATE))
-(defn get-day-of-week []
-  (.get (GregorianCalendar.) Calendar/DAY_OF_WEEK))
+  (if (= date-str "") ;handle special special case
+    {:unit Calendar/SECOND :val 0}
+    (let [tokens (tokenize date-str)]
+      {:unit (units (get-unit tokens))
+       :val  (get-time-value tokens)})))
 
 ;absolute dates
 (defn get-groups-date [date-str]
