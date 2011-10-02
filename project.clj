904c7789727e011957a97c78b812d8f37ff73f3f
Greg Sexton <gregsexton@gmail.com>
2011-10-02 16:35:19 +0100

    Added lein-daemon dependency to project.

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
                displaying text in large on your primary screen. It uses
                a client/server model and can be used to display anything
                possible from the command line."
+  :dev-dependencies [[lein-daemon "0.3.1"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
+  :daemon {:gtd {:ns gtd.core 
+                 :pidfile "gtd.pid"}}
   :main gtd.core)
